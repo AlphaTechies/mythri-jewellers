8ef8b9b7365c88e7ea5f5dd8fe3ea065afca099d
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-06-20 19:44:23 +0530

    added components in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home";
 function App() {
 
   return (
-    <div className="flex flex-col gap-2 h-screen items-center justify-center">
-      <h1 className="text-3xl">Mythri Jewellers</h1>
-      <p className="text-xl text-gray-700">Hold tight...</p>
-    </div>
-
+    // <div className="flex flex-col gap-2 h-screen items-center justify-center">
+    //   <h1 className="text-3xl">Mythri Jewellers</h1>
+    //   <p className="text-xl text-gray-700">Hold tight...</p>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </Router>
+ 
   )
 }
 
