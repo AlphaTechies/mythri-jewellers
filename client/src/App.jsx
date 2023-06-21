5a8202751d82f70bbe6a3acec087397ea2662eaa
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-21 19:29:55 +0530

    navbar somewhat completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,13 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 function App() {
-
   return (
-    // <div className="flex flex-col gap-2 h-screen items-center justify-center">
-    //   <h1 className="text-3xl">Mythri Jewellers</h1>
-    //   <p className="text-xl text-gray-700">Hold tight...</p>
-    // </div>
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
- 
-  )
+  );
 }
 
-export default App
+export default App;
