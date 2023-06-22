338d2b6d9265b7e5f0622e8289ae7b9666d66d77
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-22 08:17:56 +0530

    trending section added

---
+++ client/src/utils/products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    name: "Necklaces",
+    image: "/categories/category-1.webp",
+  },
+  {
+    name: "Earrings",
+    image: "/categories/category-3.webp",
+  },
+  {
+    name: "Bracelets",
+    image: "/categories/category-5.webp",
+  },
+  {
+    name: "Rings",
+    image: "/categories/category-2.webp",
+  },
+  {
+    name: "Pendants",
+    image: "/categories/category-6.webp",
+  },
+  {
+    name: "Anklets",
+    image: "/categories/category-7.webp",
+  },
+  {
+    name: "Bangles",
+    image: "/categories/category-4.webp",
+  },
+  {
+    name: "Brooches",
+    image: "/categories/category-8.webp",
+  },
+  {
+    name: "Charms",
+    image: "/categories/category-9.webp",
+  },
+  {
+    name: "Watches",
+    image: "/categories/category-10.webp",
+  },
+];
+
+export default products;
