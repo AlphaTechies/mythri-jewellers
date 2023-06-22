3ef066d389aa4db339fffc90aac3ec21357953af
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-22 08:23:40 +0530

    offers section added

--- client/src/components/Offers.jsx
+++ client/src/components/Offers.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import products from "../utils/products";
 
 const Offers = () => {
-  return (
-    <div>Offers</div>
-  )
-}
+  const discountedProducts = products.filter(
+    (product) => product.discountedPrice
+  );
 
-export default Offers
+  return (
+    <div className="mx-3 md:mx-10 mt-6">
+      <div className="text-center">
+        <h2 className="text-3xl font-bold text-primary mb-4">Special Offers</h2>
+        <p className="text-lg text-primary">
+          Take advantage of these exclusive discounts on selected products.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+        {discountedProducts.map((product, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center p-4 rounded-lg"
+          >
+            <div className="w-32 h-32 lg:hover:scale-110 duration-500">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="object-cover w-full h-full rounded-md"
+              />
+            </div>
+            <p className="text-center text-lg font-semibold text-primary mt-2">
+              {product.name}
+            </p>
+            <div className="flex items-center justify-center mt-2">
+              <p className="text-sm text-gray-500 line-through">
+                {product.price}
+              </p>
+              <p className="ml-2 text-xl font-semibold text-primary">
+                {product.discountedPrice}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Offers;
--- client/src/utils/products.js
+++ client/src/utils/products.js
@@ -2,42 +2,62 @@ const products = [
   {
     name: "Necklaces",
     image: "/categories/category-1.webp",
+    price: "1500 INR",
+    discountedPrice: "1200 INR",
   },
   {
     name: "Earrings",
     image: "/categories/category-3.webp",
+    price: "800 INR",
+    discountedPrice: "650 INR",
   },
   {
     name: "Bracelets",
     image: "/categories/category-5.webp",
+    price: "1000 INR",
+    discountedPrice: "800 INR",
   },
   {
     name: "Rings",
     image: "/categories/category-2.webp",
+    price: "1200 INR",
+    discountedPrice: "950 INR",
   },
   {
     name: "Pendants",
     image: "/categories/category-6.webp",
+    price: "900 INR",
+    discountedPrice: "700 INR",
   },
   {
     name: "Anklets",
     image: "/categories/category-7.webp",
+    price: "600 INR",
+    discountedPrice: "500 INR",
   },
   {
     name: "Bangles",
     image: "/categories/category-4.webp",
+    price: "1100 INR",
+    discountedPrice: "900 INR",
   },
   {
     name: "Brooches",
     image: "/categories/category-8.webp",
+    price: "700 INR",
+    discountedPrice: "550 INR",
   },
   {
     name: "Charms",
     image: "/categories/category-9.webp",
+    price: "800 INR",
+    discountedPrice: "650 INR",
   },
   {
     name: "Watches",
     image: "/categories/category-10.webp",
+    price: "2000 INR",
+    discountedPrice: "1600 INR",
   },
 ];
 
