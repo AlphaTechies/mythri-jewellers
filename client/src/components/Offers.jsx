a6a8948a3da51820c7cc96003b98ab03330be616
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-25 14:33:54 +0530

    offers section UI changed

--- client/src/components/Offers.jsx
+++ client/src/components/Offers.jsx
@@ -6,36 +6,33 @@ const Offers = () => {
   );
 
   return (
-    <div className="mx-3 md:mx-10 mt-6">
+    <div className="mx-auto w-full max-w-7xl items-center space-y-4 px-2 py-10 md:space-y-0">
       <div className="text-center">
         <h2 className="text-3xl font-bold text-primary mb-4">Special Offers</h2>
         <p className="text-lg text-primary">
           Take advantage of these exclusive discounts on selected products.
         </p>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+      <div className="grid grid-cols-2 py-10 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
         {discountedProducts.map((product, index) => (
           <div
             key={index}
-            className="flex flex-col items-center justify-center p-4 rounded-lg"
+            className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]"
           >
-            <div className="w-32 h-32 lg:hover:scale-110 duration-500">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="object-cover w-full h-full rounded-md"
-              />
-            </div>
-            <p className="text-center text-lg font-semibold text-primary mt-2">
-              {product.name}
-            </p>
-            <div className="flex items-center justify-center mt-2">
-              <p className="text-sm text-gray-500 line-through">
-                {product.price}
-              </p>
-              <p className="ml-2 text-xl font-semibold text-primary">
-                {product.discountedPrice}
-              </p>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="object-cover w-full h-full rounded-md"
+            />
+            <div className="absolute inset-0 rounded-md bg-gradient-to-t from-primary to-transparent"></div>
+            <div className="absolute text-secondary bottom-4 left-4 text-left">
+              <p className="bottom-4 left-4 text-left">{product.name}</p>
+              <div className="flex items-center justify-center mt-2">
+                <p className="text-sm line-through">{product.price}</p>
+                <p className="ml-2 text-xl font-semibold">
+                  {product.discountedPrice}
+                </p>
+              </div>
             </div>
           </div>
         ))}
